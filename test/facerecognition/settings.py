a1b3f9bfbd4bca0daa3328ef78f6bdefdedcb70a
Galuh304 <galuhapriliano30@gmail.com>
2022-11-14 21:21:56 +0700

    galuh commit

--- test/facerecognition/settings.py
+++ test/facerecognition/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'facerecognition.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'attendancesystem',
+        'USER': 'root',
+        'PASSWORD': 'admin123',
     }
 }
 
